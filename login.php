3af43d4e2a6105ec50f449fd4414de2c29986282
Brayanhnk <brayanhoinacki@gmail.com>
2026-04-29 21:18:17 -0300

    feat: Pagina de Autenticacao do login

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+if(!isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true){
+    header("Location: index.php");
+    exit();
+}
+
+$error = '';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if($username === USER_NAME && password_verify($password, USER_PASS_HASH)){
+        $_SESSION['logged_in'] = true;
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = 'Usuário ou senha inválidos.';
+    }
+}
